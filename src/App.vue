fae8fce90ecda57bf646c707174cd82e05f4641c
lzjlxebr <tyyzmyd@gmail.com>
2018-12-27 00:37:07 +0800

    add init app

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: Helvetica, sans-serif;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
 import Vue from 'vue';
-import VueI18n from 'vue-i18n'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import App from './App.vue';
-import enLocale from 'element-ui/lib/locale/lang/en'
-import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
+import './styles/main.css'
+import App from './app.vue'
+//app.vue中引入
+import $ from 'jquery'
 
-Vue.use(VueI18n)
-Vue.use(ElementUI,{ size: 'small', zIndex: 3000 });
-Vue.locale('zh-cn', zhLocale)
-Vue.locale('en', enLocale)
-
-new Vue({
-    el: '#app',
-    render: h => h(App)
-});
+window.onload = function () {
+    new Vue({
+        el: '#app',
+        render: h => h(App)
+    });
+}
