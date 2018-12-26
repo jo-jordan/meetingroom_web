2b1b76db41942fc3d629279fb24c75e6bb8c9733
lzjlxebr <tyyzmyd@gmail.com>
2018-12-27 01:40:39 +0800

    add some regular loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,201 +1,122 @@
-//webpack.config.js
-const path = require('path')
-const { VueLoaderPlugin } = require('vue-loader')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const ExtractRootCss = new ExtractTextPlugin('styles/root.css');
-const ExtractVueCss = new ExtractTextPlugin('styles/style.css');
-
-const webpack = require('webpack')
-const env = process.env.NODE_ENV
-//获取工程的版本号
-const version = process.env.npm_package_version
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
-    // entry point
-    entry: {
-        app: './src/main.js',
-    },
-
-    // output direction
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist/',
-        filename: "js/[name].js"
-    },
-    mode: 'development',
-
-    module: {
-        rules: [
-            {
-                test: /\.js$/, //用正则匹配文件，用require或者import引入的都会匹配到
-                loader: "babel-loader", //加载器名，就是上一步安装的loader
-                exclude: /node_modules/ //排除node_modules目录，我们不加载node模块中的js哦~
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    // 'postcss-loader'
-                ]
-
-                //依次使用以上loader加载css文件，postcss-loader可以暂时不加，后面再深入修改webpack配置的时候再说用处
-                //
-                //也可以写成这样 loader："style-loader!css-loader!postcss-loader"
-            },
-            {
-                test: /\.(png|jpe?j|gif|svg)(\?.*)?$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                    name: 'img/[name].[ext]?[hash]'
-                }
-                //图片文件大小小于limit的数值，就会被改写成base64直接填入url里面，
-                //不然会输出到dist/img目录下，[name]原文件名，[ext]原后缀，[hash]在url上加上一点哈希值避免缓存。
-            },
-            {
-                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-                loader: "url-loader",
-                options: {
-                    limit: 10000,
-                    name: 'fonts/[name].[ext]?[hash]'
-                }
-                //和上面一致
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-                //这一个loader当然是vue项目必须的加载器啦，不加其他规则的话，
-                //简单的这样引入就可以了，vue-loader会把vue单文件直接转成js。
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader',
-                    'less-loader'
-                ]
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    loaders: {
-                        'less': [//lang属性对应的名称
-                            'vue-style-loader',//首先给vue的样式loader过滤一遍
-                            'css-loader',//css-loader,把css转js
-                            'less-loader'//用less编译
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    loaders: {
-                        css: ExtractTextPlugin.extract({
-                            use: 'css-loader',
-                            fallback: 'vue-style-loader' // <- 这是vue-loader的依赖
-                        }),
-                        //用了less或者sass的地方都要用上哦
-                        'less': ExtractTextPlugin.extract({
-                            use: [
-                                'css-loader',
-                                'less-loader'
-                            ],
-                            fallback: 'vue-style-loader'
-                        })
-                    }
-                }
-            }
-        ]
-    },
-    resolve: {
-        //缩写扩展
-        alias: {
-            //正在使用的是vue的运行时版本，而此版本中的编译器时不可用的，我们需要把它切换成运行时 + 编译的版本
-            'vue$': 'vue/dist/vue.esm.js',// 'vue/dist/vue.common.js' for webpack 1
-            //用@直接指引到src目录下，如：'./src/main'可以写成、'@/main'
-            '@': path.resolve(__dirname, './src'),
-        },
-        //引入路径是不用写对应的后缀名
-        extensions: ['*', '.js', '.vue', '.json']
-    },
-    plugins: [
-        // make sure to include the plugin for the magic
-        new VueLoaderPlugin(),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            title: 'vue demo',
-            template: './index.html',
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        ExtractRootCss,//填入插件实例，复用的css
-        ExtractVueCss,//记得按顺序填入，vue内的css
-        new UglifyJsPlugin({
-            uglifyOptions: {
-                compress: {
-                    warnings: false
-                }
-            },
-            sourceMap: true
-        })
-    ],
-    externals: {
-        'jquery': 'window.jQuery'
-    },
-    devServer: {
-        contentBase: "./dist",
-        historyApiFallback: true,
-        noInfo: true,
-        overlay: true
-    },
-    performance: {
-        hints: false
-    },
-    devtool: '#eval-source-map',
-    optimization: {
-        splitChunks: {
-            chunks: 'async',
-            minSize: 30000,
-            minChunks: 1,
-            maxAsyncRequests: 5,
-            maxInitialRequests: 3,
-            automaticNameDelimiter: '~',
-            name: true,
-            cacheGroups: {
-                vendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    priority: -10
-                },
-                default: {
-                    minChunks: 2,
-                    priority: -20,
-                    reuseExistingChunk: true
-                }
-            }
+  entry: {
+    app: './src/main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader?indentedSyntax'
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'scss': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader'
+            ],
+            'sass': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader?indentedSyntax'
+            ]
+          }
+          // other vue-loader options go here
         }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpe?j|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'img/[name].[ext]?[hash]'
+        }
+        //图片文件大小小于limit的数值，就会被改写成base64直接填入url里面，
+        //不然会输出到dist/img目录下，[name]原文件名，[ext]原后缀，[hash]在url上加上一点哈希值避免缓存。
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: 'fonts/[name].[ext]?[hash]'
+        }
+        //和上面一致
+      },
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
     },
-    optimization: {
-        minimize: false
-    }
-
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
 }
-/*生成生产代码的时候才触发*/
-if (process.env.NODE_ENV === 'production') {
-    module.exports.devtool = '#source-map'
 
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new webpack.LoaderOptionsPlugin({
-            minimize: true
-        })
-    ])
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
 }
