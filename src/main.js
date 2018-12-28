98eaf8a01ab3feef3fa021217c2e4542b5de2403
lzjlxebr <tyyzmyd@gmail.com>
2018-12-29 00:07:33 +0800

    init template

--- src/main.js
+++ src/main.js
@@ -2,26 +2,41 @@ import Vue from 'vue'
 
 import Cookies from 'js-cookie'
 
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
 import ElementUI from 'element-ui'
-import VueRouter from 'vue-router'
 import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss' // global css
 
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 import i18n from './lang'
+import './icons' // icon
+import './errorLog' // error log
+import './permission' // permission control
+import './mock' // simulation data
+
+import * as filters from './filters' // global filters
 
 Vue.use(ElementUI, {
   size: Cookies.get('size') || 'medium', // set element-ui default size
   i18n: (key, value) => i18n.t(key, value)
 })
-Vue.use(VueRouter)
+
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
 
 Vue.config.productionTip = false
 
 const app = new Vue({
   el:'#app',
   router,
+  store,
   i18n,
   render: h => h(App)
 })
