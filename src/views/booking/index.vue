3e07d082d65358dd1d58e0f63c7e406c7cd32f07
lzjlxebr <tyyzmyd@gmail.com>
2019-01-02 01:02:44 +0800

    modify meetingroom structure

--- src/views/booking/index.vue
+++ src/views/booking/index.vue
@@ -1,91 +1,34 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-table :key="tableKey" :data="list" border fit highlight-current-row style="width: 100%;">
-        <el-table-column>
+      <el-table v-loading='listLoading' :key="tableKey" :data="list" border fit highlight-current-row style="width: 100%;">
+        <el-table-column :label="$t('table.id')" prop="id" sortable="custom" align="center" width="65">
           <template slot-scope="scope">
-            <span/>
+            <span>{{ scope.row.id }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column :label="$t('table.name')" prop="name" sortable="custom" align="center" width="100">
+          <template slot-scope="scope">
+            <span>{{ scope.row.name }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column :label="$t('table.code')" prop="code" sortable="custom" align="center" width="200">
+          <template slot-scope="scope">
+            <span>{{ scope.row.code }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column :label="$t('table.status')" prop="status" sortable="custom" align="center" width="50">
+          <template slot-scope="scope">
+            <span>{{ scope.row.status }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column :label="$t('table.createTime')" prop="createTime" sortable="custom" align="center" width="200">
+          <template slot-scope="scope">
+            <span>{{ scope.row.createTime }}</span>
           </template>
         </el-table-column>
       </el-table>
-
-      <pagination
-        v-show="total>0"
-        :total="total"
-        :page.sync="listQuery.page"
-        :limit.sync="listQuery.limit"
-        @pagination="getList"
-      />
-
-      <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
-        <el-form
-          ref="dataForm"
-          :rules="rules"
-          :model="temp"
-          label-position="left"
-          label-width="70px"
-          style="width: 400px; margin-left:50px;"
-        >
-          <el-form-item :label="$t('table.type')" prop="type">
-            <el-select v-model="temp.type" class="filter-item" placeholder="Please select">
-              <el-option
-                v-for="item in calendarTypeOptions"
-                :key="item.key"
-                :label="item.display_name"
-                :value="item.key"
-              />
-            </el-select>
-          </el-form-item>
-          <el-form-item :label="$t('table.date')" prop="timestamp">
-            <el-date-picker
-              v-model="temp.timestamp"
-              type="datetime"
-              placeholder="Please pick a date"
-            />
-          </el-form-item>
-          <el-form-item :label="$t('table.title')" prop="title">
-            <el-input v-model="temp.title"/>
-          </el-form-item>
-          <el-form-item :label="$t('table.status')">
-            <el-select v-model="temp.status" class="filter-item" placeholder="Please select">
-              <el-option v-for="item in statusOptions" :key="item" :label="item" :value="item"/>
-            </el-select>
-          </el-form-item>
-          <el-form-item :label="$t('table.importance')">
-            <el-rate
-              v-model="temp.importance"
-              :colors="['#99A9BF', '#F7BA2A', '#FF9900']"
-              :max="3"
-              style="margin-top:8px;"
-            />
-          </el-form-item>
-          <el-form-item :label="$t('table.remark')">
-            <el-input
-              :autosize="{ minRows: 2, maxRows: 4}"
-              v-model="temp.remark"
-              type="textarea"
-              placeholder="Please input"
-            />
-          </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button @click="dialogFormVisible = false">{{ $t('table.cancel') }}</el-button>
-          <el-button
-            type="primary"
-            @click="dialogStatus==='create'?createData():updateData()"
-          >{{ $t('table.confirm') }}</el-button>
-        </div>
-      </el-dialog>
-
-      <el-dialog :visible.sync="dialogPvVisible" title="Reading statistics">
-        <el-table :data="pvData" border fit highlight-current-row style="width: 100%">
-          <el-table-column prop="key" label="Channel"/>
-          <el-table-column prop="pv" label="Pv"/>
-        </el-table>
-        <span slot="footer" class="dialog-footer">
-          <el-button type="primary" @click="dialogPvVisible = false">{{ $t('table.confirm') }}</el-button>
-        </span>
-      </el-dialog>
+      <!-- <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" /> -->
     </div>
   </div>
 </template>
@@ -95,10 +38,26 @@ import { getInfo } from '@/api/meetingroom'
 
 export default {
   name: 'MeetingRoom',
+  data() {
+    return {
+        tableKey: 0,
+        list: null
+      }
+  },
   created() {
-    getInfo(this.listQuery).then(response => {
-      console.log('response', response)
-    })
+    this.getList()
+  },
+  methods:{
+    getList(){
+      this.listLoading = true
+      getInfo(this.listQuery).then(response => {
+        console.log('response', response)
+        console.log('response.data', response.data)
+        this.list = response.data.items
+
+        this.listLoading = false
+      })
+    }
   }
 }
 </script>
