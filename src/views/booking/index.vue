6543cb59506ddc5e450c8f441900f99703726396
lzjlxebr <tyyzmyd@gmail.com>
2019-01-02 01:09:22 +0800

    use attr ':min-width' of element-ui

--- src/views/booking/index.vue
+++ src/views/booking/index.vue
@@ -2,27 +2,27 @@
   <div class="app-container">
     <div class="filter-container">
       <el-table v-loading='listLoading' :key="tableKey" :data="list" border fit highlight-current-row style="width: 100%;">
-        <el-table-column :label="$t('table.id')" prop="id" sortable="custom" align="center" width="65">
+        <el-table-column :label="$t('table.id')" prop="id" sortable="custom" align="center" :min-width="65">
           <template slot-scope="scope">
             <span>{{ scope.row.id }}</span>
           </template>
         </el-table-column>
-        <el-table-column :label="$t('table.name')" prop="name" sortable="custom" align="center" width="100">
+        <el-table-column :label="$t('table.name')" prop="name" sortable="custom" align="center" :min-width="120">
           <template slot-scope="scope">
             <span>{{ scope.row.name }}</span>
           </template>
         </el-table-column>
-        <el-table-column :label="$t('table.code')" prop="code" sortable="custom" align="center" width="200">
+        <el-table-column :label="$t('table.code')" prop="code" sortable="custom" align="center" :min-width="150">
           <template slot-scope="scope">
             <span>{{ scope.row.code }}</span>
           </template>
         </el-table-column>
-        <el-table-column :label="$t('table.status')" prop="status" sortable="custom" align="center" width="50">
+        <el-table-column :label="$t('table.status')" prop="status" sortable="custom" align="center" :min-width="50">
           <template slot-scope="scope">
             <span>{{ scope.row.status }}</span>
           </template>
         </el-table-column>
-        <el-table-column :label="$t('table.createTime')" prop="createTime" sortable="custom" align="center" width="200">
+        <el-table-column :label="$t('table.createTime')" prop="createTime" sortable="custom" align="center" :min-width="200">
           <template slot-scope="scope">
             <span>{{ scope.row.createTime }}</span>
           </template>
