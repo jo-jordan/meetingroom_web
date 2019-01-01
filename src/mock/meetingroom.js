1c8abef110d692c4a5c689d73e366a5db0ea01fb
lzjlxebr <tyyzmyd@gmail.com>
2019-01-02 02:03:49 +0800

    use more realize data

--- src/mock/meetingroom.js
+++ src/mock/meetingroom.js
@@ -7,7 +7,7 @@ for (let i = 0; i < count; i++) {
   List.push(
     Mock.mock({
       id: '@increment',
-      name: '@title(5, 10)',
+      name: Mock.Random.cword(2, 5),
       code: Mock.Random.string(24),
       status: '@integer(0, 2)',
       createTime: '@datetime'
