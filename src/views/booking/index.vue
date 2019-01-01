8ebf11ffb6b9f474519af1f0b281e1c507f15070
lzjlxebr <tyyzmyd@gmail.com>
2019-01-02 02:04:26 +0800

    deal with el-tag, dispaly what i want

--- src/views/booking/index.vue
+++ src/views/booking/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-table v-loading='listLoading' :key="tableKey" :data="list" border fit highlight-current-row style="width: 100%;">
-        <el-table-column :label="$t('table.id')" prop="id" sortable="custom" align="center" :min-width="65">
+      <el-table v-loading='listLoading' :key="tableKey" :data="list" border fit highlight-current-row style="width: 100%;" :row-class-name="tableRowClassName">
+        <el-table-column :label="$t('table.id')" prop="id" sortable="custom" align="center" :min-width="40">
           <template slot-scope="scope">
             <span>{{ scope.row.id }}</span>
           </template>
@@ -12,21 +12,29 @@
             <span>{{ scope.row.name }}</span>
           </template>
         </el-table-column>
-        <el-table-column :label="$t('table.code')" prop="code" sortable="custom" align="center" :min-width="150">
+        <el-table-column :label="$t('table.code')" prop="code" sortable="custom" align="center" :min-width="100">
           <template slot-scope="scope">
             <span>{{ scope.row.code }}</span>
           </template>
         </el-table-column>
         <el-table-column :label="$t('table.status')" prop="status" sortable="custom" align="center" :min-width="50">
           <template slot-scope="scope">
-            <span>{{ scope.row.status }}</span>
+            <el-tag v-if="scope.row.status==0" type="success">{{ $t('table.'+getStatus(scope.row.status)) }}</el-tag>
+            <el-tag v-if="scope.row.status==1" type="danger">{{ $t('table.'+getStatus(scope.row.status)) }}</el-tag>
+            <el-tag v-if="scope.row.status==2" type="info">{{ $t('table.'+getStatus(scope.row.status)) }}</el-tag>
           </template>
         </el-table-column>
-        <el-table-column :label="$t('table.createTime')" prop="createTime" sortable="custom" align="center" :min-width="200">
+        <el-table-column :label="$t('table.createTime')" prop="createTime" sortable="custom" align="center" :min-width="100">
           <template slot-scope="scope">
             <span>{{ scope.row.createTime }}</span>
           </template>
         </el-table-column>
+        <el-table-column :label="$t('table.actions')" align="center" width="230" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button v-if="scope.row.status==0" size="mini" type="success" @click="handleModifyStatus(scope.row,'published')">{{ $t('table.book') }}</el-button>
+          <el-button v-if="scope.row.status!=0" size="mini" type="primary" @click="handleModifyStatus(scope.row,'published')">{{ $t('table.bookInfo') }}</el-button>
+        </template>
+      </el-table-column>
       </el-table>
       <!-- <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" /> -->
     </div>
@@ -44,6 +52,19 @@ export default {
         list: null
       }
   },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        0: 'success',
+        1: 'danger',
+        2: 'info'
+      }
+      return statusMap[status]
+    },
+    typeFilter(type) {
+      return calendarTypeKeyValue[type]
+    }
+  },
   created() {
     this.getList()
   },
@@ -57,7 +78,34 @@ export default {
 
         this.listLoading = false
       })
+    },
+    tableRowClassName({row, rowIndex}) {
+      if (row.status === 0) {
+        return 'success-row'
+      } else if (row.status === 1){
+        return 'warning-row'
+      } else if (row.status === 2){
+        return 'disabled-row'
+      }
+      return ''
+    },
+    getStatus(status){
+      return status === 0? 'available' : status === 1 ? 'reserved' : 'trimming'
     }
   }
 }
 </script>
+
+<style>
+  .el-table .warning-row {
+    background: oldlace;
+  }
+
+  .el-table .disabled-row {
+    background: rgb(235, 234, 232);
+  }
+
+  .el-table .success-row {
+    background: #f0f9eb;
+  }
+</style>
