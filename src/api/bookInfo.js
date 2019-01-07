ae714582cb1eb8cc4ea15be5e867f0861e7ddfd9
lzjlxebr <tyyzmyd@gmail.com>
2019-01-07 21:24:18 +0800

    bookinfo api

---
+++ src/api/bookInfo.js
@@ -0,0 +1,33 @@
+import request from '@/utils/request'
+
+export function getInfo(query) {
+  return request({
+    url: '/book/info/get',
+    method: 'get',
+    params: query
+  })
+}
+
+export function createBook(data) {
+  return request({
+    url: '/book/info/create',
+    method: 'post',
+    data
+  })
+}
+
+export function deleteBook(data) {
+  return request({
+    url: '/book/info/delete',
+    method: 'delete',
+    data
+  })
+}
+
+export function updateBook(data) {
+  return request({
+    url: '/book/info/update',
+    method: 'update',
+    data
+  })
+}
